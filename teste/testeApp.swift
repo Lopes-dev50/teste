1631e8a60a0b5381237fd6bdde14959469b5e317
SandroLopes <109092451+Lopes-dev50@users.noreply.github.com>
2024-01-12 14:25:39 -0300

    Initial Commit

---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by Sandro Lopes on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
